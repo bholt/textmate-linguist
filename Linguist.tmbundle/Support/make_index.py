fa5f52700f2be7078c277e1de6dea0dbd3bb3091
Brandon Holt <bholt@cs.washington.edu>
2014-04-16 18:05:33 -0700

    implement simple completion

--- Linguist.tmbundle/Support/make_index.py
+++ Linguist.tmbundle/Support/make_index.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
-import sys, os
-LIBCLANG_PATH = os.environ['TM_BUNDLE_SUPPORT']+"/libclang-py"
-# LIBCLANG_PATH = '/Users/bholt/Library/Application Support/Avian/Bundles/ClangMate.tmbundle/Support/libclang-py'
+import sys, os; from os import environ as env
+
+LIBCLANG_PATH = "./libclang-py"
+if env.has_key('TM_BUNDLE_SUPPORT'):
+  LIBCLANG_PATH = env['TM_BUNDLE_SUPPORT']+"/libclang-py"  
 sys.path.append(LIBCLANG_PATH)
-from clang.cindex import Index, Cursor, CursorKind
+from clang.cindex import Index, Cursor, CursorKind, Config
+
+Config.set_library_path('/opt/llvm/head/lib')
 
 # PROJECT_DIR = '/Users/bholt/dev/test'
 
@@ -12,7 +16,11 @@ DEBUG = False
 class MateIndex:
   
   def __init__(self):
-    self.project_dir = os.environ['TM_PROJECT_DIRECTORY']
+    if env.has_key('TM_PROJECT_DIRECTORY'):
+      self.project_dir = env['TM_PROJECT_DIRECTORY']
+    else:
+      self.project_dir = "."
+    
     self.index = Index.create()
     self.type_kinds = set([CursorKind.STRUCT_DECL, CursorKind.CLASS_DECL, CursorKind.CLASS_TEMPLATE,CursorKind.TYPEDEF_DECL])
     
@@ -34,11 +42,39 @@ class MateIndex:
     
     return types
   
+  def find(self, node=None, kinds=set(), result=list(), local_only=True):
+    global DEBUG
+    
+    if not node:
+      node = self.tu.cursor
+    # if DEBUG and node.kind.is_declaration():
+      # print node.spelling, node.kind
+      
+    if node.kind in kinds:
+      if (not local_only) or self.project_dir in str(node.location.file):
+        # print node.spelling, node.kind, node.location.file
+        result.append({
+          'name': node.spelling,
+          'file': str(node.location.file),
+          'line': node.location.line
+        })
+    
+    for c in node.get_children():
+      self.find(c, kinds, result, local_only)
+    
+    return result
+  
   def parse(self, fname):
     self.tu = self.index.parse(fname)
     
     
 if __name__ == "__main__":
   DEBUG = True
-  types = find_types(tu.cursor)
-  print types
+  import pprint
+  
+  idx = MateIndex()
+  idx.parse('test/test.cpp')
+    
+  idx.find(kinds=set([CursorKind.VAR_DECL]))
+  
+  # import ipdb; ipdb.set_trace()
